2725d33d7f91aa8f5fe44f59bd7470a20159b867
Hardik Bagdi <hbagdi1@binghamton.edu>
2016-12-12 23:17:55 -0500

    WIP heap;

--- src/data/linear/heap.go
+++ src/data/linear/heap.go
@@ -1,10 +1,12 @@
 package linear
 
-import "errors"
+import (
+	"bytes"
+	"errors"
+	"strconv"
+)
 
-//import "strconv"
-
-// Heap represents a backed backed by a slice
+// Heap represents a min heap backed backed by a slice
 type Heap struct {
 	array []int
 	count int
@@ -18,12 +20,16 @@ func NewHeap() *Heap {
 	return heap
 }
 
-//func (H *Heap) String() string {
-//	s := "Count: "
-//	s += strconv.Itoa(H.count) + "\n"
-// TODO add array to print
-//	return s
-//}
+func (H *Heap) String() string {
+
+	var buffer bytes.Buffer
+	buffer.WriteString("Count: ")
+	buffer.WriteString(strconv.Itoa(H.count) + "\n")
+	for val := range H.array[1 : H.count+1] {
+		buffer.WriteString(strconv.Itoa(val))
+	}
+	return buffer.String()
+}
 
 // Count returns the number of elements currently in the heap
 func (H *Heap) Count() int {
@@ -35,15 +41,15 @@ func (H *Heap) Array() []int {
 	return H.array[1 : H.count+1]
 }
 
-// Search determines if an element exists in the heap or not
-func (H *Heap) Search(value int) bool {
+// Search determines if an element exists in the heap or not; index return(0-based)
+func (H *Heap) Search(value int) (int, bool) {
 	//TODO improve; use tree pre-order and cut-off
 	for i := 1; i <= H.count; i++ {
 		if H.array[i] == value {
-			return true
+			return i - 1, true
 		}
 	}
-	return false
+	return 0, false
 }
 
 // ElementAt returns element at index(0-based). Sets bool if element at index exists; else false
@@ -51,14 +57,15 @@ func (H *Heap) ElementAt(index int) (int, bool) {
 	if index >= H.count {
 		return 0, false
 	}
-	return H.array[index-1], true
+	return H.array[index+1], true
 	return 0, false
 }
 
 // Insert adds the value to the heap; expanding the underlying structure if necessary
 func (H *Heap) Insert(value int) error {
 	//first search for the element; if it exists then return the error
-	if H.Search(value) {
+	_, present := H.Search(value)
+	if present {
 		return errors.New("Value already exists in the heap.")
 	}
 	//cause we don't use the first index
@@ -72,12 +79,39 @@ func (H *Heap) Insert(value int) error {
 	return nil
 }
 
-// Remove removes the value from the heap
-func (H *Heap) Remove(value int) error {
+// RemoveAt removes the element at index specified. Index is 0-based
+func (H *Heap) RemoveAt(index int) error {
+	index++
+	if index > H.count {
+		return errors.New("Index out of boud")
+	}
+	H.swap(H.count, index)
+	H.array[H.count] = 0
+	H.count--
+	H.minHeapify(index)
+	if H.count < len(H.array)/4 {
+		H.resize()
+	}
 	return nil
 }
 
-// GetRoot returns min or max depending upon the implementaion of Compare of the user defined type
+func (H *Heap) deleteRoot() error {
+	if H.count < 1 {
+		return errors.New("No element in heap")
+	}
+	return H.RemoveAt(0)
+}
+
+// Remove removes the value from the heap
+func (H *Heap) Remove(value int) error {
+	index, present := H.Search(value)
+	if !present {
+		return errors.New("Value not present in heap")
+	}
+	return H.RemoveAt(index)
+}
+
+// GetRoot returns min element
 func (H *Heap) GetRoot() (int, bool) {
 	if H.count == 0 {
 		return 0, false
@@ -86,6 +120,33 @@ func (H *Heap) GetRoot() (int, bool) {
 	return H.array[1], true
 }
 
+// DecreaseKey reduces the key to a new value
+func (H *Heap) DecreaseKey(oldValue, newValue int) error {
+	if newValue >= oldValue {
+		return errors.New("New value cannot be greater than or equal to old value")
+	}
+	index, present := H.Search(oldValue)
+	if !present {
+		return errors.New("Value not present in heap")
+	}
+	H.array[index] = newValue
+	H.swimUp(index)
+	return nil
+}
+
+// IncreaseKey increases  the value of a key
+func (H *Heap) IncreaseKey(oldValue, newValue int) error {
+	if newValue <= oldValue {
+		return errors.New("New value cannot be less than or equal to old value")
+	}
+	index, present := H.Search(oldValue)
+	if !present {
+		return errors.New("Value not present in heap")
+	}
+	H.array[index] = newValue
+	H.sinkDown(index)
+	return nil
+}
 func (H *Heap) resize() {
 	var newArray []int
 	if H.count+1 == len(H.array) {
@@ -115,5 +176,28 @@ func (H *Heap) swimUp(index int) {
 	}
 }
 
+func (H *Heap) left(index int) int {
+	return 2*index + 1
+}
+func (H *Heap) right(index int) int {
+	return 2*index + 2
+}
+func (H *Heap) minHeapify(index int) {
+	l := H.left(index)
+	r := H.right(index)
+	min := index
+	if l < H.count+1 && H.array[min] > H.array[l] {
+		min = l
+	}
+	if r < H.count+1 && H.array[min] > H.array[r] {
+		min = r
+	}
+	if index != min {
+		H.swap(min, index)
+		H.minHeapify(min)
+	}
+}
+
 func (H *Heap) sinkDown(index int) {
+	H.minHeapify(index)
 }
